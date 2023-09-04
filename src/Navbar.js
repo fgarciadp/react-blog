5b098868b5e2ba101f94884755e70a46fbfbe28f
Felipe Garcia <22felipe95@gmail.com>
2023-09-04 13:11:07 -0400

    running local json server

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>React Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                }}>New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
